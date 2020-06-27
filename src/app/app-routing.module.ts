d39bbe3179711de74238562d47bba4dc0e8d8a70
keder3009 <keder3009@gmail.com>
2020-06-27 14:19:01 -0500

    actualizacion de proyecto a angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from "./modules/inicio/inicio.component";
+
+
+const routes: Routes = [
+
+  {path: '', component: InicioComponent},
+
+
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
